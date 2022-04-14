476a6ea68c1bcd05ceab6e69ba87989cc952c8f4
Abhishek P <abhishekkuttikkol007@gmail.com>
2022-04-14 05:20:00 +0000

    refactor: test

--- src/Components/AddPets.js
+++ src/Components/AddPets.js
@@ -112,7 +112,7 @@ const Addpets = () => {
           <div className="max-w-md w-full space-y-8">
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sell Pets
+                Sell Pets in petskaro
               </h2>
             </div>
             <form onSubmit={handleSubmit} className="mt-8 space-y-6">
